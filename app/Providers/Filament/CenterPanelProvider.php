99bc8ae188431456489a907142c4b2420f03e31a
LAITH343 <87765501+LAITH343@users.noreply.github.com>
2026-05-27 15:49:54 +0300

    remove FilamentInfoWidget and AccountWidget and add missing key in docker compose

--- app/Providers/Filament/CenterPanelProvider.php
+++ app/Providers/Filament/CenterPanelProvider.php
@@ -29,8 +29,6 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\PreventRequestForgery;
@@ -58,10 +56,6 @@ class CenterPanelProvider extends PanelProvider
                 Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Center/Widgets'), for: 'App\Filament\Center\Widgets')
-            ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
-            ])
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
